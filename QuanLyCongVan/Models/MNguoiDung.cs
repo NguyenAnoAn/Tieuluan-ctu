c1a1d75061ac99b7e3f1eb9df4bcde061195f21f
Nguyen Van Khoi <ngvankhoi@gmail.com>
2014-10-12 23:43:37 +0700

    Signed-off-by: Nguyen Van Khoi <ngvankhoi@gmail.com>

---
+++ QuanLyCongVan/Models/MNguoiDung.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace QuanLyCongVan.Models
+{
+    public class MNguoiDung
+    {
+
+    }
+    public class NguoiDungDBContext : DbContext
+    {
+        public DbSet<MNguoiDung> DSNguoiDung { get; set; }
+    }
+}
