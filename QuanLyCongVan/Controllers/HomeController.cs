3f7cc031b2b649ed2f39bf9458280f3b627cde23
Nguyen Van Khoi <ngvankhoi@gmail.com>
2014-10-16 00:15:23 +0700

    Signed-off-by: Nguyen Van Khoi <ngvankhoi@gmail.com>

--- QuanLyCongVan/Controllers/HomeController.cs
+++ QuanLyCongVan/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace QuanLyCongVan.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = " Thông tin mời họp";
 
             return View();
         }
