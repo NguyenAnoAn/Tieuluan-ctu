f60646e5ad931e00042012d3d38a99aef8190347
Nguyen Van Khoi <ngvankhoi@gmail.com>
2014-10-28 23:35:44 +0700

    Signed-off-by: Nguyen Van Khoi <ngvankhoi@gmail.com>

--- Helper/SHA1.cs
+++ Helper/SHA1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace QuanLyCongVan.Helper
+namespace QuanLyCongVan.Helpers
 {
     public class SHA1
     {
---
+++ QuanLyCongVan/Helpers/KiemTraQuyen.cs
@@ -0,0 +1,30 @@
+﻿using QuanLyCongVan.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuanLyCongVan.Helpers
+{
+    public class KiemTraQuyen
+    {
+        /// <summary>
+        /// kiem tra quyền người dùng được lưu trữ trong biến UInt32 có được cấp các quyền được hệ thống hay không.
+        /// </summary>
+        /// <param name="quyen">Biến lưu trữ quyền của người dùng </param>
+        /// <param name="cocacquyen">danh sách các quyền cần kiểm tra </param>
+        /// <returns></returns>
+        public static bool KiemTraCoQuyen(UInt32 quyen,params QuyenSuDung[] cocacquyen)
+        {
+            bool ret = true;
+            foreach (QuyenSuDung sq in cocacquyen)
+            {
+                ret &= ((quyen & (UInt32)sq) == (UInt32)sq);
+                if (!ret)
+                    break;
+            }
+            return ret;
+        }
+        public static UInt32 ADMIN { get { return 0xffffffff; } }
+    }
+}
